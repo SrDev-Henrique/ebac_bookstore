ea4b7b4b11a572ccb76533fdf9867454080953e5
SrDev-Henrique <halbuquerque2850@gmail.com>
2026-08-01 14:28:23 -0300

    feat: wip

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,8 +1,4 @@
-from collections.abc import Sequence
-
 from django.db.models import QuerySet
-from rest_framework.authentication import BaseAuthentication, BasicAuthentication, SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from product.models.product import Product
@@ -10,11 +6,5 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet[Product]):
-    authentication_classes: Sequence[type[BaseAuthentication]] = [
-        SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-    ]
-    permission_classes: Sequence[type[BasePermission]] = [IsAuthenticated]
     serializer_class: type[ProductSerializer] = ProductSerializer
     queryset: QuerySet[Product] = Product.objects.all()
